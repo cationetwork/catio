34d2c616a953117658345eaf0f4e2f8298b7f294
Grover Chou <groverchout@gmail.com>
2018-03-07 03:51:16 +0800

    Update iptables.sh

--- shell/iptables.sh
+++ shell/iptables.sh
@@ -1,8 +1,11 @@
-# NMNET project iptables.sh version 1
-# New CatIO network shell version1
-# Wan
+#!/bin/sh
+#
+# CatIO Network iptables setting version 1
+#
+# WAN IPv4 address
 wan=
 
+# Clean up
 iptables -F
 iptables -X
 iptables -t nat -F
@@ -10,14 +13,17 @@ iptables -t nat -X
 iptables -P INPUT DROP
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
-# ACCEPT port
-iptables -A INPUT  -p tcp -m multiport --dports 22,53,80,443,12450 -j ACCEPT
-iptables -A INPUT  -p udp -m multiport --dports 22,53,80,443,12450 -j ACCEPT
-# ACCEPT IO
+
+# Accept ports
+iptables -A INPUT -p tcp -m multiport --dports 22,53,80,443,12450 -j ACCEPT
+iptables -A INPUT -p udp -m multiport --dports 22,53,80,443,12450 -j ACCEPT
+
+# Accept lo
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
-# Log save 
+
+# Log save
 iptables -A INPUT -i $wan -j LOG --log-prefix "iptables dropped packets:"
 iptables -t nat -A POSTROUTING -o $wan -j MASQUERADE
